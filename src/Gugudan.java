c79f0fc9af59948b66e7446985a0966a507f5c04
unul09 <39664609+unul09@users.noreply.github.com>
2021-06-27 10:12:00 +0900

    구구단/ 실행 클래스와 메소드 클래스의 분리

--- src/Gugudan.java
+++ src/Gugudan.java
@@ -22,11 +22,6 @@ public class Gugudan {
         System.out.println();
     }
 
-    public static void main(String[] args) {
-        for (int j = 2; j < 10; j++) {
-            // calculate와 print 메소드 활용해 구구단 구현하기
-        	// calculate메소드가 배열을 return 하므로, print의 인자로 넣어준다
-        	print(calculate(j));
-        }
-    }
+   
+    
 }
