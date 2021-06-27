1494f480759e2daecaf988de70d7307d4f491e6c
unul09 <39664609+unul09@users.noreply.github.com>
2021-06-27 10:24:36 +0900

    사용자가 입력한 값에 따라 크기가 다른 구구단을 계산해 출력한다. 문자열 입력

--- src/GugudanMain.java
+++ src/GugudanMain.java
@@ -5,14 +5,19 @@ public class GugudanMain {
 		
 		Scanner s = new Scanner(System.in);
 		
-		System.out.print("구구단의 크기를 입력하세요: ");
-		int a = s.nextInt();
+		System.out.print("입력할 크기를 결정할 문자열을 입력하세요: ");
+		String a = s.next();
+		
+		String [] splited = a.split(",");
+		
+		int first = Integer.parseInt(splited[0]);
+		int second = Integer.parseInt(splited[1]);
 		
 		
 		
 		
-		for (int j = 2; j < a+1; j++) {        
-        	Gugudan.print(Gugudan.calculate(j,a));
+		for (int j = 2; j < first+1; j++) {        
+        	Gugudan.print(Gugudan.calculate(j,second));
         }
 
 	}
